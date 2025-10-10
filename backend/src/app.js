3bbaa06cb9cdcda26c2578490d6da31abc36b704
Leonardo Gerasimenko Testa <29783433+theovniboss@users.noreply.github.com>
2025-10-10 11:33:21 -0300

    Inicio de Desenvolvimento

---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import router from "./router/index.js";
+
+const app = express();
+app.use(express.json());
+
+app.use('/api', router);
+
+
+export default app;
---
+++ backend/src/models/users.js
@@ -0,0 +1,57 @@
+import db from "../config/db.js";
+
+const getAllUsers = async () => {
+    const res = await db.query('SELECT * FROM users');
+    return res.rows;
+}
+
+const getUserById = async (id) => {
+    const res = await db.query('SELECT * FROM users WHERE userid = $1', [id]);
+    return res.rows[0];
+}
+
+const insertUser = async (user) => {
+    const { email, password, firstname, lastname, dateofbirth, identifyproviderid  } = user;
+    const query = `
+    INSERT INTO users (
+        email, 
+        password, 
+        firstname, 
+        lastname, 
+        dateofbirth, 
+        identifyproviderid
+    ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
+
+    const res = await db.query(query, [email, password, firstname, lastname, dateofbirth, identifyproviderid]);
+    return res.rows[0];
+}   
+
+const updateUser = async (id, user) => {
+    const { email, password, firstname, lastname, dateofbirth, identifyproviderid  } = user;
+    const query = `UPDATE users SET
+        email = $1,
+        password = $2,
+        firstname = $3,
+        lastname = $4,
+        dateofbirth = $5,
+        identifyproviderid = $6,
+        updatedat = CURRENT_TIMESTAMP
+        WHERE userid = $7 RETURNING *`;
+    const res = await db.query(query, [email, password, firstname, lastname, dateofbirth, identifyproviderid, id]);
+    return res.rows[0];
+}
+
+const deleteUser = async (id) => {
+    const res = await db.query('DELETE FROM users WHERE userid = $1', [id]);
+    return res.rowCount > 0;
+}
+
+
+
+export default { 
+    getAllUsers,
+    getUserById,
+    insertUser,
+    updateUser,
+    deleteUser
+};
