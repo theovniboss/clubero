6d407cbd799ce54cb3fc6c8674a08a5dc7bfb3d9
Leonardo Gerasimenko Testa <29783433+theovniboss@users.noreply.github.com>
2025-10-14 18:12:01 -0300

    Dev

--- frontend/web-app/src/components/Header.vue
+++ frontend/web-app/src/components/Header.vue
@@ -6,13 +6,18 @@
 		<section class="flex justify-center items-center p-5 grow">
 
 			<section class="flex ml-auto" v-if="isAuthenticated">
-				<div class="h-10 v-10 rounded-full border-1 border-gray-300 p-2 bg-white mr-4 hover:bg-gray-50">
+				<div class="h-10 v-10 rounded-full border-1 border-gray-300 p-2 bg-white mr-4 hover:bg-gray-50 duration-300">
 					<font-awesome-icon icon="fa-regular fa-bell" class="h-6 w-6 text-gray-500"  />
 				</div>
-				<picture>
+				<picture class="h-10 w-10 cursor-pointer">
 					<img :src="user.picture" :alt="user.name" class="h-10 w-10 rounded-full border-gray-300 border-1" />
-				</picture>	
-
+				</picture>
+				<div class="flex flex-col justify-center ml-3 mr-6">
+					<span class="text-sm font-medium text-gray-700">{{ user.name }}</span>
+					<span class="text-xs text-gray-500">{{ user.email }}</span>
+					<button @click="userLogout" class="h-10 px-2 bg-red-500 text-white rounded-md hover:bg-danger-600 duration-300">Sair</button>
+				</div>
+				
 			</section>
 
 		</section>
